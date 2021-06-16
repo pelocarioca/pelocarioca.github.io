95d4b523697e2d7c1b624dfe4d3280ab601d14f0
Cassiopea <cassiopea.acebes@gmail.com>
2021-06-16 12:09:56 +0100

    Mejora del inventario, posibilidad de cambiar el nombre del inventario

--- ansible/crear-inventario.sh
+++ ansible/crear-inventario.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Nombre del archivo: crear-inventario.sh
 
+file="hosts"
 read -p "Nombre del aula (ASIR1): " aula
 aula=${aula:-ASIR1}
 
@@ -10,10 +11,10 @@ sobre=${sobre:-N}
 
 case $sobre in
 	Y*|y*)
-		aula2=$(echo "[$aula]" > inventario.ini)
+		aula2=$(echo "[$aula]" > $file)
 		resul;;
 	N*|n*)
-	     	aula2=$(echo "[$aula]" >> inventario.ini)
+	     	aula2=$(echo "[$aula]" >> $file)
 		resul;;
 	C*|c*)
 		exit;;
@@ -27,7 +28,7 @@ function resul {
 echo "Buscando IPs con el puerto 22 abierto"
 $aula2
 dev=$(ip route get 8.8.8.8 | grep "dev *" | cut -d" " -f 5)
-nmap -p 22 --open -n $(nmcli dev show $dev | grep "^IP4\.ADDRESS.*:" | tr -s " " | cut -d" " -f2) | grep "^Nmap scan" | cut -d" " -f5 >> inventario.ini
+nmap -p 22 --open -n $(nmcli dev show $dev | grep "^IP4\.ADDRESS.*:" | tr -s " " | cut -d" " -f2) | grep "^Nmap scan" | cut -d" " -f5 >> $file
 exit 0
 }
 elec
